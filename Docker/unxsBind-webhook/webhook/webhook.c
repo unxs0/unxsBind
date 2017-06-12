59a962926526d47cf4542eb4081c02433c677889
root <root@localhost.localdomain>
2017-06-12 15:25:39 -0400

    Testing pre-alpha

--- Docker/unxsBind-webhook/webhook/webhook.c
+++ Docker/unxsBind-webhook/webhook/webhook.c
@@ -16,38 +16,75 @@ HELP
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+
+void logfileLine(FILE *fp,const char *cFunction,const char *cLogline)
+{
+	time_t luClock;
+	char cTime[32];
+	pid_t pidThis;
+	const struct tm *tmTime;
+
+	pidThis=getpid();
+
+	time(&luClock);
+	tmTime=localtime(&luClock);
+	strftime(cTime,31,"%b %d %T",tmTime);
+
+	fprintf(fp,"%s webhook[%u]::%s %s.\n",cTime,pidThis,cFunction,cLogline);
+	fflush(fp);
+
+}//void logfileLine()
+
+
+void RunForkedCommand(char *cCommand, FILE *fp)
+{
+	pid_t pidChild;
+
+	pidChild=fork();
+	if(pidChild!=0)
+		return;
+
+	system(cCommand);
+	logfileLine(fp,"RunForkedCommand",cCommand);
+
+}//void RunForkedCommand(char *cCommand)
 
 
 int main(int iArgc, char *cArgv[])
 {
 	char *cGitVersion="GitVersion:"GitVersion;
-	pentry entries[256];
-	entry gentries[8];
-	char *gcl;
-	int cl=0;
-	char gcHost[100]={""};
-	char gcHostname[100]={""};
-	register int x;
+	char cHost[100]={""};
+	char cHostname[100]={""};
 
 	FILE *fp=NULL;
 	fp=fopen("/var/www/unxs/logs/cgi.log","a");
+	logfileLine(fp,"main","start");
 
-	gethostname(gcHostname,98);
+	gethostname(cHostname,98);
 
 	if(getenv("REMOTE_HOST")!=NULL)
-		sprintf(gcHost,"%.99s",getenv("REMOTE_HOST"));
+		sprintf(cHost,"%.99s",getenv("REMOTE_HOST"));
 
 	else if(getenv("REMOTE_ADDR")!=NULL)
-		sprintf(gcHost,"%.99s",getenv("REMOTE_ADDR"));
+		sprintf(cHost,"%.99s",getenv("REMOTE_ADDR"));
 
 	if(getenv("HTTP_X_REAL_IP")!=NULL)
-		sprintf(gcHost,"%.99s",getenv("HTTP_X_REAL_IP"));
+		sprintf(cHost,"%.99s",getenv("HTTP_X_REAL_IP"));
 
 	//Post with URL get items
 	if(getenv("REQUEST_METHOD")!=NULL)
 	{
+		register int x;
+		char cRedeploy[100]={""};
+		//pentry entries[256];
+		//int cl=0;
+		entry gentries[8];
+		char *gcl;
+
 		printf("Content-type: text/text\n\n");
-		gcl = getenv("QUERY_STRING");
+		gcl=getenv("QUERY_STRING");
 
 		for(x=0;gcl[0] != '\0' && x<8;x++)
 		{
@@ -59,17 +96,31 @@ int main(int iArgc, char *cArgv[])
 			escape_shell_cmd(gentries[x].val);
 		}
 		for(x=0;gentries[x].name[0]&&x<8;x++)
-			fprintf(fp,"%s=%s\n",gentries[x].name,gentries[x].val);
+		{
+			logfileLine(fp,"name",gentries[x].name);
+			logfileLine(fp,"value",gentries[x].val);
+			if(!strcmp(gentries[x].name,"redeploy"))
+				sprintf(cRedeploy,"%.99s",gentries[x].val);
+		}
 
-		printf("Thanks! %s %s\n",gcHost,gcHostname);
+		printf("Thanks! %s %s\n",cHost,cHostname);
+		if(cRedeploy[0])
+		{
+			char cCommand[256];
+			sprintf(cCommand,"/usr/bin/docker rm -f %.99s",cRedeploy);
+			RunForkedCommand(cCommand,fp);
+		}
 	}
 	else
 	{
 		printf("Non-cgi %s %s\n",cArgv[0],cGitVersion);
+		logfileLine(fp,"main","Non-cgi");
 	}
 
 	if(fp) fclose(fp);
+	logfileLine(fp,"main","end");
 	return(0);
 
 }//end of main()
+
 
